e6073920da07499ddc5ec58b67545698595c5321
Darpan Sheth <113262001+darpanSheth@users.noreply.github.com>
2023-01-28 13:03:04 -0600

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ContosoPets.models;
+
+
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+    
+    public virtual List<Product> Products { get; } = new List<Product>();
+}
